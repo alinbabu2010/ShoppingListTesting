62a8dd8ab46a92c8dfe8b4a0ca3400aec169c211
Alin Babu <alinbabu2010@gmail.com>
2022-11-21 09:31:10 +0530

    Add viewBinding for MainActivity.kt

--- app/src/main/java/com/sample/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/sample/shoppinglist/MainActivity.kt
@@ -2,10 +2,15 @@ package com.sample.shoppinglist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.sample.shoppinglist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
