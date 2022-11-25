5e3e42117093fc9a13d2019ae8c694399c24a0be
Alin Babu <alinbabu2010@gmail.com>
2022-11-25 12:23:22 +0530

    Add viewModel for shopping

---
+++ app/src/main/java/com/sample/shoppinglist/ui/ShoppingViewModel.kt
@@ -0,0 +1,55 @@
+package com.sample.shoppinglist.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sample.shoppinglist.data.local.ShoppingItem
+import com.sample.shoppinglist.data.models.ImageResponse
+import com.sample.shoppinglist.data.models.Resource
+import com.sample.shoppinglist.data.repositories.ShoppingRepository
+import com.sample.shoppinglist.utils.Event
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ShoppingViewModel @Inject constructor(
+    private val repository: ShoppingRepository
+) : ViewModel() {
+
+    val shoppingItems = repository.observeAllShoppingItems()
+
+    val totalPrice = repository.observeTotalPrice()
+
+    private val _images = MutableLiveData<Event<Resource<ImageResponse>>>()
+    val images: LiveData<Event<Resource<ImageResponse>>> = _images
+
+    private val _currentImageUrl = MutableLiveData<String>()
+    val currentImageUrl: LiveData<String> = _currentImageUrl
+
+    private val _insertShoppingItemStatus = MutableLiveData<Event<Resource<ShoppingItem>>>()
+    val insertShoppingItemStatus: LiveData<Event<Resource<ShoppingItem>>> =
+        _insertShoppingItemStatus
+
+    fun setCurImageUrl(url: String) {
+        _currentImageUrl.postValue(url)
+    }
+
+    fun deleteShoppingItem(shoppingItem: ShoppingItem) = viewModelScope.launch {
+        repository.deleteShoppingItem(shoppingItem)
+    }
+
+    fun insertShoppingItemIntoDb(shoppingItem: ShoppingItem) = viewModelScope.launch {
+        repository.insertShoppingItem(shoppingItem)
+    }
+
+    fun insertShoppingItem(name: String, amountString: String, priceString: String) {
+
+    }
+
+    fun searchForImage(imageQuery: String) {
+
+    }
+
+}
