f6e9d1c95dd517540192399fbf3f81a5f94ad4f9
Alin Babu <alinbabu2010@gmail.com>
2022-11-25 11:03:17 +0530

    Add constants object

---
+++ app/src/main/java/com/sample/shoppinglist/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.sample.shoppinglist.utils
+
+object Constants {
+
+    const val API_ENDPOINT = "/api/"
+    const val PARAM_QUERY = "q"
+    const val PARAM_KEY = "key"
+
+    const val DATABASE_NAME = "shopping_db"
+    const val BASE_URL = "https://pixabay.com"
+
+}
