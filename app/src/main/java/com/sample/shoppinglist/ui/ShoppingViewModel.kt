1902721702d3382b7bd58d4bb7f54ee64d87a611
Alin Babu <alinbabu2010@gmail.com>
2022-11-28 11:44:38 +0530

    Add functionality to `insertShoppingItem` and `searchForImage` function in ShoppingViewModel.kt

--- app/src/main/java/com/sample/shoppinglist/ui/ShoppingViewModel.kt
+++ app/src/main/java/com/sample/shoppinglist/ui/ShoppingViewModel.kt
@@ -4,10 +4,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.sample.shoppinglist.R
 import com.sample.shoppinglist.data.local.ShoppingItem
 import com.sample.shoppinglist.data.models.ImageResponse
 import com.sample.shoppinglist.data.models.Resource
 import com.sample.shoppinglist.data.repositories.ShoppingRepository
+import com.sample.shoppinglist.utils.Constants
 import com.sample.shoppinglist.utils.Event
 import com.sample.shoppinglist.utils.UIString
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -47,11 +49,63 @@ class ShoppingViewModel @Inject constructor(
     }
 
     fun insertShoppingItem(name: String, amountString: String, priceString: String) {
-
+        if (name.isEmpty() || amountString.isEmpty() || priceString.isEmpty()) {
+            _insertShoppingItemStatus.postValue(
+                Event(Resource.error(errorString.message(R.string.empty_filed_error)))
+            )
+            return
+        }
+        if (name.length > Constants.MAX_NAME_LENGTH) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        errorString.message(
+                            R.string.max_name_error,
+                            Constants.MAX_NAME_LENGTH
+                        )
+                    )
+                )
+            )
+            return
+        }
+        if (priceString.length > Constants.MAX_PRICE_LENGTH) {
+            _insertShoppingItemStatus.postValue(
+                Event(
+                    Resource.error(
+                        errorString.message(
+                            R.string.max_price_error,
+                            Constants.MAX_PRICE_LENGTH
+                        )
+                    )
+                )
+            )
+            return
+        }
+        val amount = try {
+            amountString.toInt()
+        } catch (exception: Exception) {
+            _insertShoppingItemStatus.postValue(
+                Event(Resource.error(errorString.message(R.string.valid_amount_msg)))
+            )
+            return
+        }
+        val shoppingItem = ShoppingItem(
+            name = name, amount = amount,
+            price = priceString.toFloat(),
+            imageUrl = _currentImageUrl.value ?: ""
+        )
+        insertShoppingItemIntoDb(shoppingItem)
+        setCurImageUrl("")
+        _insertShoppingItemStatus.postValue(Event(Resource.success(shoppingItem)))
     }
 
     fun searchForImage(imageQuery: String) {
-
+        if (imageQuery.isEmpty()) return
+        _images.value = Event(Resource.loading(null))
+        viewModelScope.launch {
+            val response = repository.searchForImage(imageQuery)
+            _images.value = Event(response)
+        }
     }
 
 }
