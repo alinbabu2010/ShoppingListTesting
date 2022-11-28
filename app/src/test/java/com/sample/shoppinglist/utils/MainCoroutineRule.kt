493c6b1caf223b5fde20cf52984435888fe54085
Alin Babu <alinbabu2010@gmail.com>
2022-11-28 11:42:27 +0530

    Add liveData extension function and coroutine rule in test package

---
+++ app/src/test/java/com/sample/shoppinglist/utils/MainCoroutineRule.kt
@@ -0,0 +1,29 @@
+package com.sample.shoppinglist.utils
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@ExperimentalCoroutinesApi
+class MainCoroutineRule(
+    private val dispatcher: CoroutineDispatcher = StandardTestDispatcher()
+) : TestWatcher(), CoroutineScope by TestScope(dispatcher) {
+
+    override fun starting(description: Description) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+        Dispatchers.resetMain()
+    }
+
+}
