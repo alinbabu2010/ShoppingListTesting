7d72a2d8ad44016112757c53e3ec3c5de2474e62
Alin Babu <alinbabu2010@gmail.com>
2022-12-02 18:14:48 +0530

    Add constructor parameter for viewModel and adapter and function to observe livedata
    Also add add shopping item swipe to delete functionality and refactor viewModel instantiation in ShoppingFragment.kt

--- app/src/main/java/com/sample/shoppinglist/ui/fragments/ShoppingFragment.kt
+++ app/src/main/java/com/sample/shoppinglist/ui/fragments/ShoppingFragment.kt
@@ -5,17 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.LEFT
+import androidx.recyclerview.widget.ItemTouchHelper.RIGHT
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.sample.shoppinglist.R
 import com.sample.shoppinglist.databinding.FragmentShoppingBinding
+import com.sample.shoppinglist.ui.adapters.ShoppingItemAdapter
 import com.sample.shoppinglist.ui.viewModel.ShoppingViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class ShoppingFragment : Fragment() {
+class ShoppingFragment @Inject constructor(
+    private val shoppingItemAdapter: ShoppingItemAdapter,
+    var viewModel: ShoppingViewModel? = null
+) : Fragment() {
 
     private lateinit var binding: FragmentShoppingBinding
-    private val viewModel by viewModels<ShoppingViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,10 +40,63 @@ class ShoppingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = viewModel ?: ViewModelProvider(requireActivity())[ShoppingViewModel::class.java]
+
+        subscribeToObservers()
+        setupRecyclerView()
+
         binding.fabAddShoppingItem.setOnClickListener {
             findNavController().navigate(
                 ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
             )
+        }
+
+    }
+
+    private val itemTouchCallback = object : ItemTouchHelper.SimpleCallback(0, LEFT or RIGHT) {
+
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ) = true
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val pos = viewHolder.layoutPosition
+            val item = shoppingItemAdapter.currentList[pos]
+            viewModel?.deleteShoppingItem(item)
+            Snackbar.make(
+                requireView(),
+                R.string.delete_success_msg,
+                Snackbar.LENGTH_LONG
+            ).apply {
+                setAction(R.string.undo) {
+                    viewModel?.insertShoppingItemIntoDb(item)
+                }
+                show()
+            }
+        }
+
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvShoppingItems.apply {
+            adapter = shoppingItemAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            ItemTouchHelper(itemTouchCallback).attachToRecyclerView(this)
+        }
+    }
+
+    private fun subscribeToObservers() {
+
+        viewModel?.shoppingItems?.observe(viewLifecycleOwner) {
+            shoppingItemAdapter.submitList(it)
+        }
+
+        viewModel?.totalPrice?.observe(viewLifecycleOwner) {
+            val price = it ?: 0F
+            val priceText = "Total price: ₹$price"
+            binding.tvShoppingItemPrice.text = priceText
         }
 
     }
