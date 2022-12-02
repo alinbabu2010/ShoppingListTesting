eefe9254a70006711f7bdd4538b5d6e867b9f1e8
Alin Babu <alinbabu2010@gmail.com>
2022-12-02 18:15:57 +0530

    Add constructor parameter for shoppingItemAdapter and condition for ShoppingFragment in instantiate

--- app/src/main/java/com/sample/shoppinglist/ui/fragments/ShoppingFragmentFactory.kt
+++ app/src/main/java/com/sample/shoppinglist/ui/fragments/ShoppingFragmentFactory.kt
@@ -4,10 +4,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.bumptech.glide.RequestManager
 import com.sample.shoppinglist.ui.adapters.ImageAdapter
+import com.sample.shoppinglist.ui.adapters.ShoppingItemAdapter
 import javax.inject.Inject
 
 class ShoppingFragmentFactory @Inject constructor(
     private val imageAdapter: ImageAdapter,
+    private val shoppingItemAdapter: ShoppingItemAdapter,
     private val glide: RequestManager
 ) : FragmentFactory() {
 
@@ -15,6 +17,7 @@ class ShoppingFragmentFactory @Inject constructor(
         return when (className) {
             ImagePickFragment::class.java.name -> ImagePickFragment(imageAdapter)
             AddShoppingItemFragment::class.java.name -> AddShoppingItemFragment(glide)
+            ShoppingFragment::class.java.name -> ShoppingFragment(shoppingItemAdapter)
             else -> super.instantiate(classLoader, className)
         }
     }
