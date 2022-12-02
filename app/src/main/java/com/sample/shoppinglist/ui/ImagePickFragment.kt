c0dae540676825cb411b5c4dfffc0e54a8ac19b1
Alin Babu <alinbabu2010@gmail.com>
2022-12-02 13:35:00 +0530

    Add imageAdapter parameter and setup recyclerView for loading image grids in ImagePickFragment.kt

--- app/src/main/java/com/sample/shoppinglist/ui/ImagePickFragment.kt
+++ app/src/main/java/com/sample/shoppinglist/ui/ImagePickFragment.kt
@@ -6,14 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.sample.shoppinglist.databinding.FragmentImagePickBinding
+import com.sample.shoppinglist.ui.adapters.ImageAdapter
+import com.sample.shoppinglist.utils.Constants.GRID_SPAN_COUNT
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class ImagePickFragment : Fragment() {
+class ImagePickFragment @Inject constructor(
+    val imageAdapter: ImageAdapter
+) : Fragment() {
 
     private lateinit var binding: FragmentImagePickBinding
-    private val viewModel by viewModels<ShoppingViewModel>()
+    val viewModel by viewModels<ShoppingViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +29,22 @@ class ImagePickFragment : Fragment() {
     ): View {
         binding = FragmentImagePickBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        imageAdapter.setOnItemClickListener {
+            findNavController().popBackStack()
+            viewModel.setCurImageUrl(it)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvImages.apply {
+            adapter = imageAdapter
+            layoutManager = GridLayoutManager(requireContext(), GRID_SPAN_COUNT)
+        }
     }
 
 }
