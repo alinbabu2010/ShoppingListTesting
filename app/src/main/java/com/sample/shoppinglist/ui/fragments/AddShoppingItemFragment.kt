f66546c93b1efaa1c3589934bf75668182041490
Alin Babu <alinbabu2010@gmail.com>
2022-12-02 16:31:33 +0530

    Add constructor parameter for glide and function to observe livedata and show snackBar
    Also add add shopping item button click action and refactor viewModel instantiation

--- app/src/main/java/com/sample/shoppinglist/ui/fragments/AddShoppingItemFragment.kt
+++ app/src/main/java/com/sample/shoppinglist/ui/fragments/AddShoppingItemFragment.kt
@@ -6,17 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.RequestManager
+import com.google.android.material.snackbar.Snackbar
+import com.sample.shoppinglist.R
+import com.sample.shoppinglist.data.models.Resource.Status.*
 import com.sample.shoppinglist.databinding.FragmentAddShoppingItemBinding
 import com.sample.shoppinglist.ui.viewModel.ShoppingViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class AddShoppingItemFragment : Fragment() {
+class AddShoppingItemFragment @Inject constructor(
+    private val glide: RequestManager
+) : Fragment() {
 
     private lateinit var binding: FragmentAddShoppingItemBinding
-    val viewModel by viewModels<ShoppingViewModel>()
+    lateinit var viewModel: ShoppingViewModel
 
     private val onBackPressedCallback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
@@ -37,14 +44,57 @@ class AddShoppingItemFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.ivShoppingImage.setOnClickListener {
-            findNavController().navigate(
-                AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment()
-            )
+        viewModel = ViewModelProvider(requireActivity())[ShoppingViewModel::class.java]
+        subscribeToObservers()
+
+        binding.run {
+
+            btnAddShoppingItem.setOnClickListener {
+                viewModel.insertShoppingItem(
+                    etShoppingItemName.text.toString(),
+                    etShoppingItemAmount.text.toString(),
+                    etShoppingItemPrice.text.toString()
+                )
+            }
+
+            ivShoppingImage.setOnClickListener {
+                findNavController().navigate(
+                    AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment()
+                )
+            }
+
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(onBackPressedCallback)
 
+    }
+
+    private fun subscribeToObservers() {
+
+        viewModel.currentImageUrl.observe(viewLifecycleOwner) {
+            glide.load(it).into(binding.ivShoppingImage)
+        }
+
+        viewModel.insertShoppingItemStatus.observe(viewLifecycleOwner) {
+            it.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    SUCCESS -> showSnackBar(R.string.added_item)
+                    ERROR -> showSnackBar(result.message ?: R.string.unknown_error)
+                    LOADING -> { /* no-op */
+                    }
+                    EMPTY_RESPONSE -> showSnackBar(R.string.no_images)
+                }
+            }
+        }
+
+    }
+
+    private inline fun <reified T> showSnackBar(value: T) {
+        val message = when (T::class) {
+            Int::class -> getString(value as Int)
+            else -> value as String
+        }
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG).show()
     }
 
 }
