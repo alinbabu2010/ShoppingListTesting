a0d1de99901b1953a00b625faeabd39caa05b4f6
Alin Babu <alinbabu2010@gmail.com>
2022-12-02 19:22:12 +0530

    Add fragmentFactory to activity

--- app/src/main/java/com/sample/shoppinglist/ui/activity/MainActivity.kt
+++ app/src/main/java/com/sample/shoppinglist/ui/activity/MainActivity.kt
@@ -3,15 +3,21 @@ package com.sample.shoppinglist.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sample.shoppinglist.databinding.ActivityMainBinding
+import com.sample.shoppinglist.ui.fragments.ShoppingFragmentFactory
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportFragmentManager.fragmentFactory = fragmentFactory
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
