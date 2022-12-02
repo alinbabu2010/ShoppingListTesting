e45151b6c0226a1fec2819529d5d96d553db16a2
Alin Babu <alinbabu2010@gmail.com>
2022-12-02 18:17:23 +0530

    Add test case for swipe to delete shopping item

--- app/src/androidTest/java/com/sample/shoppinglist/ui/ShoppingFragmentTest.kt
+++ app/src/androidTest/java/com/sample/shoppinglist/ui/ShoppingFragmentTest.kt
@@ -1,14 +1,22 @@
 package com.sample.shoppinglist.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeLeft
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import com.sample.shoppinglist.R
+import com.sample.shoppinglist.data.local.ShoppingItem
+import com.sample.shoppinglist.ui.adapters.ShoppingItemAdapter
 import com.sample.shoppinglist.ui.fragments.ShoppingFragment
 import com.sample.shoppinglist.ui.fragments.ShoppingFragmentDirections
+import com.sample.shoppinglist.ui.viewModel.ShoppingViewModel
+import com.sample.shoppinglist.utils.getOrAwaitValue
 import com.sample.shoppinglist.utils.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -18,6 +26,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -25,7 +34,13 @@ import org.mockito.Mockito.verify
 class ShoppingFragmentTest {
 
     @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
     var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var fragmentFactory: TestShoppingFragmentFactory
 
     @Before
     fun setUp() {
@@ -42,6 +57,23 @@ class ShoppingFragmentTest {
         verify(navController).navigate(
             ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
         )
+    }
+
+    @Test
+    fun swipeShoppingItemDeleteItemInDb() {
+        var testViewModel: ShoppingViewModel? = null
+        val shoppingItem =
+            ShoppingItem(id = 1, name = "Name", amount = 5, price = 5.5F, imageUrl = "")
+        launchFragmentInHiltContainer<ShoppingFragment>(fragmentFactory = fragmentFactory) {
+            viewModel?.insertShoppingItemIntoDb(shoppingItem)
+            testViewModel = viewModel
+        }
+        onView(withId(R.id.rvShoppingItems)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<ShoppingItemAdapter.ShoppingItemViewHolder>(
+                0, swipeLeft()
+            )
+        )
+        assertThat(testViewModel?.shoppingItems?.getOrAwaitValue()).isEmpty()
     }
 
 }
